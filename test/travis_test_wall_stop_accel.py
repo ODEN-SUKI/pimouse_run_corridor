e20984b8d0b458807911a49eff44c2dc48b7e7e9
ODEN-SUKI <38833158+ODEN-SUKI@users.noreply.github.com>
2018-06-14 19:02:56 +0900

    Update travis_test_wall_stop_accel.py

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -22,17 +22,17 @@ class WallStopTest(unittest.TestCase):
         return left,right
 
     def test_io(self):
-        left, right = self.set_and_get(400,100,100,0)
+        self.set_and_get(400,100,100,0)
         self.assertTrue(left == right == 0,"can't stop")
         
-        left, right = self.set_and_get(40,0,0,9)
+        self.set_and_get(40,0,0,9)
         self.assertTrue(0 < left == right < 1000, "can't move again")
         
         time.sleep(5.0)
-        left, right = self.set_and_get(40,0,0,9)
+        self.set_and_get(40,0,0,9)
         self.assertTrue(2000 < left == right, "can't accerelate")
         
-        left, right =self.set_and_get(15,0,20,15)
+        self.set_and_get(15,0,20,15)
         self.assertTrue(left == right == 0, "can't stop again")
         
         
